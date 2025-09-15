7ab76e3bd184dc0618c89a5d89335f28ea6c7ab7
priyamprasad421-del <priyamprasad421@gmail.com>
2025-09-15 20:23:47 +0000

    variables

---
+++ 01_first/01_variables.js
@@ -0,0 +1,8 @@
+const accountId =1444553
+let acccountEmail ="priyamprasad539@gmail.com"
+var accountpassword ="12345"
+accountCity = "Kolkata"
+
+// accountId =2
+console.log(accountId);
+console.table([accountId, acccountEmail, accountpassword, accountCity]);
